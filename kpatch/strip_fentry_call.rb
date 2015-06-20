5a49e0c0a9068e420c76cde104ce1d86edb330fb
valkjsaaa <jackieyang51@gmail.com>
2015-06-20 14:16:39 +0800

    added comment for ruby script

--- kpatch/strip_fentry_call.rb
+++ kpatch/strip_fentry_call.rb
@@ -1,5 +1,9 @@
 #!/usr/bin/env ruby
 
+# replace the fentry call at the beginning of each function to nops
+# supply objdump command and path to kernel for this to work
+
+# if argument is not 2, show help text
 if ARGV.count != 2
   puts "Usage: #{$0} <objdump> <kernel>"
   exit 1
@@ -7,8 +11,12 @@ end
 
 OBJDUMP = ARGV[0]
 KERNEL = ARGV[1]
+
+# virtual memory addresses and .text segment addresses
 TEXT_VMA, TEXT_OFF = `#{OBJDUMP} -h #{KERNEL} | awk '$2 == ".text" { print $4, $6 }'`.split.map(&:hex)
+# symbol table
 SYMTAB = `#{OBJDUMP} -t #{KERNEL} | awk '$3 == "F" { print $1, $6 }'`
+
 KERNEL_FILE = open(KERNEL, "r+b")
 
 SYMTAB.each_line do |line|
